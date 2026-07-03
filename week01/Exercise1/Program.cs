3a8943ee3789102e8c913f14d79abad5e6fc645b
medina2402 <adrianj.2402@gmail.com>
2026-07-03 20:03:44 -0300

    add

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("first i need your first name.");
+        string firstname = Console.ReadLine();
+        Console.WriteLine($"Hello, {firstname}!");
+        Console.WriteLine("now i need your last name.");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}!");
+
     }
 }
