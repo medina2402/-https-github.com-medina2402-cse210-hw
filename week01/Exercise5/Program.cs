3c095e11825107873a181026724965125c1f4e4e
medina2402 <adrianj.2402@gmail.com>
2026-07-04 17:36:54 -0300

    finish

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,44 @@
 using System;
 
-class Program
+internal class NewBaseType
 {
+    static string GetName()
+    {
+        Console.Write("Enter your name: ");
+        return Console.ReadLine();
+    }
+
+    static int GetNumber()
+    {
+        Console.Write("Enter a number: ");
+        return int.Parse(Console.ReadLine());
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Welcome();
+
+        string name = GetName();
+        int number = GetNumber();
+
+        Result(name, Square(number));
     }
+
+    static void Result(string name, int square)
+    {
+        Console.WriteLine($"{name}, your number squared is {square}.");
+    }
+
+    static int Square(int number)
+    {
+        return number * number;
+    }
+
+    static void Welcome()
+    {
+        Console.WriteLine("Welcome!");
+    }
+}
+
+class Program : NewBaseType
+{
 }
