b9ce987afc56aa9b046bf9eeb66cb408ab02ea68
medina2402 <adrianj.2402@gmail.com>
2026-07-04 03:10:37 -0300

    atualizado

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Your letter grade is A.");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("Your letter grade is B.");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Your letter grade is C.");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("Your letter grade is D.");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F.");
+        }
     }
-}
+}   
