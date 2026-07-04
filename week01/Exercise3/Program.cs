5f54ea44a456ee63ca15eeb6a458a10da0263e16
medina2402 <adrianj.2402@gmail.com>
2026-07-04 13:23:20 -0300

    finish

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Hello this is a Magic Number Game.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+                break;
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low.");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Too high.");
+            }
+            
+        }
+
     }
 }
