d225bbf2ac92b4e7603b17771feca5acb27b69f0
medina2402 <adrianj.2402@gmail.com>
2026-07-06 18:16:41 -0300

    updated

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,12 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-        Console.WriteLine("first i need your first name.");
-        string firstname = Console.ReadLine();
-        Console.WriteLine($"Hello, {firstname}!");
-        Console.WriteLine("now i need your last name.");
-        string lastname = Console.ReadLine();
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}!");
 
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
